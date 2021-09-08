4e74c988e0641fc02221d01763d2944c8b4c0596
VinhH2402 <78180414+VinhH2402@users.noreply.github.com>
2021-09-08 13:05:44 -0700

    complete etl data

--- db/index.js
+++ db/index.js
@@ -1,4 +1,4 @@
-const db = require('../models/index.js')
+const db = require('../models/index.js');
 const Sequelize = db.Sequelize;
 const sequelize = db.sequelize;
 
@@ -11,20 +11,10 @@ const Reviews = sequelize.define('reviews', {
   body: Sequelize.TEXT,
   recommend: Sequelize.STRING(10),
   reported: Sequelize.STRING(10),
-  reviewer_name: Sequelize.INTEGER,
-  reviewer_email: Sequelize.INTEGER,
+  name: Sequelize.STRING,
+  email: Sequelize.STRING,
   response: Sequelize.TEXT,
   helpfulness: Sequelize.INTEGER
-});
-
-const Reviewers = sequelize.define('reviewers', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
 });
 
 const Photos = sequelize.define('photos', {
@@ -45,7 +35,7 @@ const Characteristics = sequelize.define('characteristics', {
   },
   product_id: Sequelize.INTEGER,
   name: Sequelize.CHAR(20)
-})
+});
 
 const Char_reviews = sequelize.define('char_reviews', {
   id: {
@@ -58,30 +48,12 @@ const Char_reviews = sequelize.define('char_reviews', {
   value: Sequelize.INTEGER
 });
 
-const tests = sequelize.define('tests', {
-  id: { type: Sequelize.INTEGER, primaryKey: true },
-  product_id: Sequelize.INTEGER,
-  rating: Sequelize.INTEGER,
-  date: Sequelize.STRING,
-  summary: Sequelize.STRING,
-  body: Sequelize.TEXT,
-  recommend: Sequelize.BOOLEAN,
-  reported: Sequelize.BOOLEAN,
-  response: Sequelize.TEXT,
-  helpfulness: Sequelize.INTEGER,
-  reviewer: Sequelize.INTEGER
-});
-
 Reviews.sync();
-Reviewers.sync();
 Photos.sync();
 Char_reviews.sync();
 Characteristics.sync();
-tests.sync();
 
 exports.Reviews = Reviews;
-exports.Reviewers = Reviewers;
 exports.Photos = Photos;
 exports.Characteristics = Characteristics;
 exports.Char_reviews = Char_reviews;
-exports.tests = tests;
