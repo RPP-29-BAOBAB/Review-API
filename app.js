15ddfb06fba252529ed07a8465aef8a4107fb28e
VinhH2402 <78180414+VinhH2402@users.noreply.github.com>
2021-09-21 14:27:59 -0700

    complete api

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('./server.js');
 
 const server = app.listen(5000, () => {
-  console.log('Review API listening at http://localhost: 6000');
+  console.log('Review API listening at http://localhost:5000');
 });
 
 module.exports = server;
