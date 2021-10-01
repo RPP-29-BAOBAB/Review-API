ba20aceff66a05933812136a2cf3ed8ed39d0e2c
VinhH2402 <78180414+VinhH2402@users.noreply.github.com>
2021-09-30 18:49:02 -0700

    edit sql to SDC

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const env = process.env.NODE_ENV || 'production';
+const env = process.env.NODE_ENV || 'SDC';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
